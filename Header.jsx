dd3a421c14f6545896ab5baf03ffb79bf3b4c95c
KiyashN <153452219+KiyashN@users.noreply.github.com>
2023-12-21 01:02:00 +0600

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='holder'>
+        <header className='header'>
+            <Navbar />
+            <div className='header-content flex flex-c text-center text-white'>
+                <h2 className='header-title text-capitalize' >welcome reader
+                <svg xmlns="http://www.w3.org/2000/svg"  width="48" height="48" fill="currentColor" class="bi bi-emoji-smile" viewBox="-5 -5 21 21" >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                  <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"/>
+                </svg>
+                 </h2> <br/>
+                <p className='header-text fs-18 fw-3' >There you can find your book</p>
+                <SearchForm />
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Header
